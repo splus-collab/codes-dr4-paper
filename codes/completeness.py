2d7cd1cdaee2921ebbed5b22b62f3b236ae99e33
Gabriel <42849978+edr0k@users.noreply.github.com>
2023-12-14 00:44:23 -0300

    Update completeness.py

--- codes/completeness.py
+++ codes/completeness.py
@@ -19,7 +19,7 @@ pd.set_option('display.max_columns', None)
 from matplotlib import rc
 plt.rcParams["font.family"] = "serif"
 plt.rcParams["mathtext.fontset"] = "dejavuserif"
-plt.rcParams["figure.figsize"] = (13,11)
+plt.rcParams["figure.figsize"] = (15,11)
 plt.rcParams["font.size"] = 18
 plt.rcParams["xtick.direction"] = "in"
 plt.rcParams["ytick.direction"] = "in"
@@ -27,37 +27,80 @@ plt.rcParams["xtick.minor.visible"] = True
 plt.rcParams["ytick.minor.visible"] = True
 plt.rcParams["xtick.labelsize"] = 13
 plt.rcParams["ytick.labelsize"] = 13
-mpl.rcParams['axes.linewidth'] = 1.5
-mpl.rcParams.update({'font.size': 20, 'font.family': 'STIXGeneral', 'mathtext.fontset': 'stix'})
-mpl.rcParams['axes.linewidth'] = 2.0
+mpl.rcParams.update({'font.size': 16, 'font.family': 'STIXGeneral', 'mathtext.fontset': 'stix'})
+mpl.rcParams['axes.linewidth'] = 1.0
 
 colors = {
-    'i': (97, 0, 0),
-    'r': (255, 99, 0),
-    'g': (0, 195, 255),
-    'z': (97, 0, 0),
-    'u': (97, 0, 97),
-    'j0378': (97, 0, 97),
-    'j0395': (127, 0, 157),
-    'j0410': (126, 0, 217),
-    'j0430': (64, 0, 255),
-    'j0515': (22, 255, 0),
-    'j0660': (232, 0, 0),
-    'j0861': (97, 0, 0)
+    'i': (193,5,0),
+    'r': (254,95,3),
+    'g': (5,195,255),
+    'z': (151,0,8),
+    'u': (219,3,174),
+    'J0378': (195,2,188),
+    'J0395': (164,8,199),
+    'J0410': (142,2,231),
+    'J0430': (61,1,232),
+    'J0515': (32,240,17),
+    'J0660': (246,1,8),
+    'J0861': (178,0,4)
 }
+
+
+# read tables from data folder
+
+psf = Table.read('data/detections_in_bands_psf.csv')
+single = Table.read('data/detections_in_bands_single.csv')
+dual = Table.read('data/detections_in_bands.csv')
+field = Table.read('data/detections_in_bands_field.csv')
 
 # make 4x3 grid using gridspec
 
-fig = plt.figure(constrained_layout=True, figsize=(11, 5), dpi=100)
+fig = plt.figure(constrained_layout=True, figsize=(16, 8), dpi=100)
 gs = fig.add_gridspec(nrows=3, ncols=4) #, height_ratios=[1], width_ratios=[2,2,2])
 # gs.update(left=0.08, right=0.98, bottom=0.10, top=0.98, wspace=0.4, hspace=0.03)
 
+# creating subplots for the 12 bands
+ax_list = []
 # add loop for plots in gs
 for row in range(3):
     for col in range(4):
-        ax = fig.add_subplot(gs[row, col])
-        # label = 'Width: {}\nHeight: {}'.format(widths[col], heights[row])
-        # ax.annotate(label, (0.1, 0.5), xycoords='axes fraction', va='center')
+        ax_list.append(fig.add_subplot(gs[row, col]))
+
+for i,band in enumerate(['u','J0378','J0395','J0410','J0430','g','J0515','r','J0660','i','J0861','z']):
+    band2r_psf = []
+    band2r_single = []
+    band2r_dual = []
+    # band2r_field = []
+    for limit in range(14,24):
+        band2r_psf.append(psf[(psf['limit_range']==limit) & (psf['band']==band)]['count']/psf[(psf['limit_range']==limit) & (psf['band']=='r')]['count'])
+        band2r_single.append(single[(single['limit_range']==limit) & (single['band']==band)]['count']/single[(single['limit_range']==limit) & (single['band']=='r')]['count'])
+        band2r_dual.append(dual[(dual['limit_range']==limit) & (dual['band']==band)]['count']/dual[(dual['limit_range']==limit) & (dual['band']=='r')]['count'])
+        # band2r_field.append(field[(field['limit_range']==limit) & (field['band']==band)]['count']/field[(field['limit_range']==limit) & (field['band']=='r')]['count'])
+        
+    ax_list[i].plot(range(14,24), band2r_psf, label="psf", color=np.array(colors[band])/255, ls='--', lw=3, marker='s', markersize=8)
+    ax_list[i].plot(range(14,24), band2r_single, label="single", color=np.array(colors[band])/255, ls=':', lw=3, marker='^', markersize=8)
+    ax_list[i].plot(range(14,24), band2r_dual, label='dual', color=np.array(colors[band])/255, lw=3, ls='-', marker='o', markersize=8)
+    # ax_list[i].plot(range(14,24), band2r_field, label=[band+"_dual", band+"_single", band+"_psf"], color=np.array(colors[band])/255, lw=3, ls='-.', marker='s', markersize=8)
+
+    # ax_list[i].set_ylabel('completeness')
+    # ax_list[i].set_xlabel('R')
+    
+    if i % 4 == 0:
+        ax_list[i].set_ylabel('completeness')
+    
+    if i >= 8:
+        ax_list[i].set_xlabel('R')
+    
+    ax_list[i].text(14.5,0.5,band)
+    ax_list[i].legend(loc='lower left', ncol=1, frameon=False, fontsize=10)
+    ax_list[i].set_xlim(14,23)
+    ax_list[i].set_ylim(0.,1.1)
+    ax_list[i].set_xticks(np.arange(14,24,2))
+    ax_list[i].set_yticks(np.arange(0,1.1,0.2))
+    ax_list[i].grid(ls='--', alpha=0.5)
+    ax_list[i].set_axisbelow(True)
+    plt.show()
+
 
 plt.savefig('plots/completeness.png', dpi=100)
 plt.close()
