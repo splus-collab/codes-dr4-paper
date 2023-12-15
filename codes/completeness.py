25e0caa975cc867ba5e12662e4aa5d3532e472c2
schwarzam <gustavo.b.schwarz@gmail.com>
2023-12-15 03:35:01 -0300

    finished completness plot and count unique id code

--- codes/completeness.py
+++ codes/completeness.py
@@ -48,15 +48,15 @@ colors = {
 
 # read tables from data folder
 
-psf = Table.read('data/detections_in_bands_psf.csv')
-single = Table.read('data/detections_in_bands_single.csv')
-dual = Table.read('data/detections_in_bands.csv')
-field = Table.read('data/detections_in_bands_field.csv')
+psf = Table.read('../data/detections_in_bands_psf.csv')
+single = Table.read('../data/detections_in_bands_single.csv')
+dual = Table.read('../data/detections_in_bands.csv')
 
 # make 4x3 grid using gridspec
 
-fig = plt.figure(constrained_layout=True, figsize=(16, 8), dpi=100)
-gs = fig.add_gridspec(nrows=3, ncols=4) #, height_ratios=[1], width_ratios=[2,2,2])
+fig = plt.figure(constrained_layout=True, figsize=(16, 9), dpi=300)
+gs = fig.add_gridspec(nrows=3, ncols=4, wspace=0.1, hspace=0.1) #, height_ratios=[1], width_ratios=[2,2,2])
+
 # gs.update(left=0.08, right=0.98, bottom=0.10, top=0.98, wspace=0.4, hspace=0.03)
 
 # creating subplots for the 12 bands
@@ -76,11 +76,12 @@ for i,band in enumerate(['u','J0378','J0395','J0410','J0430','g','J0515','r','J0
         band2r_single.append(single[(single['limit_range']==limit) & (single['band']==band)]['count']/single[(single['limit_range']==limit) & (single['band']=='r')]['count'])
         band2r_dual.append(dual[(dual['limit_range']==limit) & (dual['band']==band)]['count']/dual[(dual['limit_range']==limit) & (dual['band']=='r')]['count'])
         # band2r_field.append(field[(field['limit_range']==limit) & (field['band']==band)]['count']/field[(field['limit_range']==limit) & (field['band']=='r')]['count'])
-        
-    ax_list[i].plot(range(14,24), band2r_psf, label="psf", color=np.array(colors[band])/255, ls='--', lw=3, marker='s', markersize=8)
-    ax_list[i].plot(range(14,24), band2r_single, label="single", color=np.array(colors[band])/255, ls=':', lw=3, marker='^', markersize=8)
-    ax_list[i].plot(range(14,24), band2r_dual, label='dual', color=np.array(colors[band])/255, lw=3, ls='-', marker='o', markersize=8)
-    # ax_list[i].plot(range(14,24), band2r_field, label=[band+"_dual", band+"_single", band+"_psf"], color=np.array(colors[band])/255, lw=3, ls='-.', marker='s', markersize=8)
+    
+    ax_list[i].plot(range(14,24), band2r_dual, label='dual', color=np.array(colors[band])/255, lw=2, ls='-', marker='o', markersize=6)
+    ax_list[i].plot(range(14,24), band2r_single, label="single", color=np.array(colors[band])/255, ls=':', lw=2, marker='^', markersize=6)
+    ax_list[i].plot(range(14,24), band2r_psf, label="psf", color=np.array(colors[band])/255, ls='--', lw=2, marker='s', markersize=6)
+
+        # ax_list[i].plot(range(14,24), band2r_field, label=[band+"_dual", band+"_single", band+"_psf"], color=np.array(colors[band])/255, lw=3, ls='-.', marker='s', markersize=8)
 
     # ax_list[i].set_ylabel('completeness')
     # ax_list[i].set_xlabel('R')
@@ -93,14 +94,15 @@ for i,band in enumerate(['u','J0378','J0395','J0410','J0430','g','J0515','r','J0
     
     ax_list[i].text(14.5,0.5,band)
     ax_list[i].legend(loc='lower left', ncol=1, frameon=False, fontsize=10)
-    ax_list[i].set_xlim(14,23)
+    ax_list[i].set_xlim(13.5,23.5)
     ax_list[i].set_ylim(0.,1.1)
     ax_list[i].set_xticks(np.arange(14,24,2))
     ax_list[i].set_yticks(np.arange(0,1.1,0.2))
     ax_list[i].grid(ls='--', alpha=0.5)
     ax_list[i].set_axisbelow(True)
-    plt.show()
+    ax_list[i].tick_params(axis='x', labelsize=12)  # Set x-ticks font size
+    ax_list[i].tick_params(axis='y', labelsize=12)  # Set y-ticks font size
 
 
-plt.savefig('plots/completeness.png', dpi=100)
+plt.savefig('../plots/completeness.png', dpi=100)
 plt.close()
