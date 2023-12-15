3d8199e5a2453dbbff5e0498bbe3dcd7fefab721
herpichfr <fabiorafaelh@gmail.com>
2023-12-15 16:58:49 +0000

    Implemente parallel dask reading dataframes. Update magxerror for paper

--- plot_photxerror.py
+++ plot_photxerror.py
@@ -9,6 +9,8 @@ import dask.dataframe as dd
 import itertools
 import multiprocessing as mp
 
+os.environ["DASK_MEMORY_LIMIT"] = "32G"
+
 
 def load_args():
     """Parse command line arguments."""
@@ -23,6 +25,12 @@ def load_args():
     parser.add_argument('--nprocs', type=int,
                         help='Number of processes to use. Default: 1.',
                         default=1)
+    parser.add_argument('--npoints', type=int,
+                        help='Number of points to plot. Use -1 to plot the entire table. Default: 10000',
+                        default=10000)
+    parser.add_argument('--nbins', type=int,
+                        help='Number of bins for shadow regions. Default: 20.',
+                        default=20)
     parser.add_argument('--plot', action='store_true',
                         help='Plot the photometry.')
     parser.add_argument('--savefig', action='store_true',
@@ -44,7 +52,7 @@ def load_csv_data(csv_file):
     data['Field'] = [field]*len(data)
     data.columns = ['RA', 'DEC', 'mag', 'e_mag', 'Field']
 
-    return data
+    return dd.from_pandas(data, npartitions=1)
 
 
 def process_csvfiles(workdir, modes, filters, nprocs, args):
@@ -57,11 +65,11 @@ def process_csvfiles(workdir, modes, filters, nprocs, args):
         list_csvs = glob.glob(os.path.join(workdir, mode, f'*_{f}.csv'))
 
         pool = mp.Pool(nprocs)
-        df = pool.map(load_csv_data, list_csvs)
+        ddfs = pool.map(load_csv_data, list_csvs)
         pool.close()
         pool.join()
 
-        alldf = pd.concat(df, ignore_index=True)
+        alldf = dd.concat(ddfs, ignore_index=True)
 
         print(f'Saving file {outputcsv}.')
         alldf.to_csv(outputcsv, index=False)
@@ -73,6 +81,9 @@ def plot_photometry(workdir, modes, filters, args):
                'j0410': 'b', 'j0430': 'dodgerblue',  'j0515': 'lime',
                'g': 'turquoise', 'r': 'limegreen', 'j0660': 'y',
                'i': 'darkorange', 'j0861': 'orangered', 'z': 'darkred'}
+    mode_names = {'dual_auto': 'Dual', 'dual_PStotal': 'PStotal',
+                  'single_auto': 'Single', 'psf_psf': 'PSF'}
+
     fig, ax = plt.subplots(12, 4, figsize=(10, 12))
     ax = ax.ravel()
     iter_modes_filters = itertools.product(filters, modes)
@@ -81,11 +92,21 @@ def plot_photometry(workdir, modes, filters, args):
         allcsv = os.path.join(mode_dir, f'all_{f}.csv')
         print(i, f, m, allcsv)
         if os.path.exists(allcsv):
-            try:
-                df = dd.read_csv(allcsv, blocksize=25e6).head(n=10000)
-            except ValueError:
-                print(f'An error occurred while reading the file {allcsv}.')
-                continue
+            if args.npoints == -1:
+                try:
+                    df = dd.read_csv(allcsv, blocksize=25e6)
+                except ValueError:
+                    print(
+                        f'An error occurred while reading the file {allcsv}.')
+                    continue
+            else:
+                try:
+                    df = dd.read_csv(allcsv, blocksize=25e6).head(
+                        n=args.npoints)
+                except ValueError:
+                    print(
+                        f'An error occurred while reading the file {allcsv}.')
+                    continue
         else:
             print(f'File {allcsv} not found.')
             continue
@@ -94,41 +115,78 @@ def plot_photometry(workdir, modes, filters, args):
         df['mag'] = df['mag'].round(3)
         df['e_mag'] = df['e_mag'].round(3)
 
-        mask = (df['mag'] > 0) & (df['mag'] < 30)
-        mask &= (df['e_mag'] > 0) & (df['e_mag'] < 30)
+        mask = (df['mag'] > 10) & (df['mag'] < 30)
+        mask &= (df['e_mag'] > 0) & (df['e_mag'] < 10.9)
+        completeness = len(df[mask]) / len(df)
+        xlims = [10.1, 26.9]
+        ylims = [-0.5, 10.9]
+        xticks = [12, 16, 20, 24]
+        yticks = [1, 3, 5, 7, 9]
 
         df = df[mask]
-        df['mag_bin'] = pd.cut(df['mag'], bins=20, labels=False)
-        percentiles = [.05, .50, .95]
-        mag_bins = df.groupby('mag_bin')['mag', 'e_mag'].quantile(
+        df = df.sort_values(by='mag')
+        df['mag_bin'] = pd.qcut(df['mag'], q=25, labels=False)
+        percentiles = [.16, .50, .84]
+        mag_bins = df.groupby('mag_bin')[['mag', 'e_mag']].quantile(
             percentiles).unstack()
+        mag_intersect = mag_bins['mag'][percentiles[1]][
+            mag_bins['e_mag'][percentiles[1]] < 0.3619].max()
 
         ax[i].scatter(df['mag'], df['e_mag'], s=1, alpha=0.4, color='gray')
-        ax[i].plot(mag_bins['mag'][0.5], mag_bins['e_mag'][0.5], 'o-', lw=2,
-                   color=colours[f])
-        ax[i].fill_between(mag_bins['mag'][0.5], mag_bins['e_mag'][0.05],
-                           mag_bins['e_mag'][0.95], color=colours[f], alpha=0.3)
+        print(min(df['mag']), max(df['mag']), min(df['e_mag']),
+              max(df['e_mag']))
+        ax[i].plot(mag_bins['mag'][percentiles[0]],
+                   mag_bins['e_mag'][percentiles[0]],
+                   'o-', lw=2, color=colours[f], ms=2)
+        ax[i].fill_between(mag_bins['mag'][percentiles[0]],
+                           mag_bins['e_mag'][percentiles[0]],
+                           mag_bins['e_mag'][percentiles[2]],
+                           color=colours[f], alpha=0.3)
+        if mag_intersect <= max(df['mag']):
+            ax[i].axvline(mag_intersect, color='k', ls='--', lw=1)
+            if i in [3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47]:
+                ax[i].text(mag_intersect+0.1, 0.05, r'$\mathrm{%.2f}$' %
+                           mag_intersect, fontsize=6)
+            else:
+                ax[i].text(mag_intersect+0.1, 9, r'$\mathrm{%.2f}$' %
+                           mag_intersect, fontsize=6)
+
         if i in [44, 45, 46, 47]:
-            ax[i].set_xlabel('mag')
+            ax[i].set_xlabel(r'$\mathrm{Mag}$', fontsize=12)
         else:
             ax[i].set_xticklabels(ax[i].get_xticklabels(), visible=False)
         if i in [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44]:
-            ax[i].set_ylabel('e_mag')
-        else:
-            ax[i].set_yticklabels(ax[i].get_yticklabels(), visible=False)
+            ax[i].set_ylabel(r'$\mathrm{e_{Mag}}$', fontsize=12)
         if i in [3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47]:
             ax2 = ax[i].twinx()
-            ax2.set_ylabel(f'{f}', rotation=90, labelpad=15)
+            if f in ['u', 'g', 'r', 'i', 'z']:
+                fname = f'{f}'
+            else:
+                fname = f'{f}'.upper()
+            ax2.set_ylabel(r"$%s$" % fname, rotation=90,
+                           labelpad=10, fontsize=12)
             ax2.set_yticklabels(ax2.get_yticklabels(), visible=False)
-            ax[i].set_yticklabels(ax[i].get_yticklabels(), visible=False)
-        if i in [0, 1, 2, 3]:
-            ax[i].set_title(f'{m}')
+            ax2.set_yticks([])
+            ax[i].set_xlim(10.1, 23)
+            ax[i].set_xticks(xticks)
+            ax[i].set_ylim(-0.02, 0.51)
+        else:
+            ax[i].set_xlim(xlims)
+            ax[i].set_ylim(ylims)
+            ax[i].set_xticks(xticks)
+            ax[i].set_yticks(yticks)
 
-        ax[i].set_xlim(10.1, 24.9)
-        ax[i].set_ylim(-0.01, 1.9)
+        if i in [0, 1, 2, 3]:
+            ax[i].set_title(f'{mode_names[m]}', fontsize=12)
+
+        ax[i].grid(alpha=0.5)
+        bbox_props = dict(boxstyle="round", fc="w", ec="0.5", alpha=0.9)
+        ax[i].text(0.05, 0.9, r'%i (%.3f)' % (len(df), completeness),
+                   transform=ax[i].transAxes,
+                   fontsize=6, verticalalignment='top', bbox=bbox_props)
 
     plt.tight_layout()
-    plt.subplots_adjust(wspace=0.0, hspace=0.0)
+    plt.subplots_adjust(wspace=0.2, hspace=0.02)
     if args.savefig:
         figpath = os.path.join(workdir, 'dr4-photxerrors.png')
         print(f'Saving figure {figpath}.')
@@ -140,13 +198,9 @@ def plot_photometry(workdir, modes, filters, args):
 def main(args):
 
     workdir = args.work_dir
-    # fields_list = args.fields_list
     modes = ['dual_auto', 'dual_PStotal', 'single_auto', 'psf_psf']
-    # filters = ['u', 'g', 'r', 'i', 'z', 'j0378', 'j0395', 'j0410', 'j0430',
-    #            'j0515', 'j0660', 'j0861']
     filters = ['u', 'j0378', 'j0395', 'j0410', 'j0430', 'g',  'j0515', 'r',
                'j0660', 'i', 'j0861', 'z']
-    # fields = pd.read_csv(os.path.join(workdir, fields_list))
     nprocs = args.nprocs
 
     process_csvfiles(workdir, modes, filters, nprocs, args)
