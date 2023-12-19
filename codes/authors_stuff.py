eb652fd1a4687a8bda139fd34818ce40168918d1
herpichfr <fabiorafaelh@gmail.com>
2023-12-19 00:47:10 +0000

    Prepare list of authors for A&A LaTex template

--- codes/authors_stuff.py
+++ codes/authors_stuff.py
@@ -5,9 +5,11 @@ import json
 authors_json = json.load(open('config/list_authors.json', 'r'))
 
 authors = authors_json['authors']
-# order authors by the key position knowing that authors is a dict
-authors = sorted(authors.items(), key=lambda x: x[1]['position'])
-# each author has a key 'institution' that is a list of institutions. Need to order them by apperance and enumerate them starting in 1
+
+# order authors by the value of the key 'position' if position != 99 or by the value of the key 'fullname' if position == 99
+authors = sorted(authors.items(), key=lambda x: (
+    x[1].get('position', 99), x[1]['fullname']))
+
 institutions = []
 for author in authors:
     for institution in author[1]['institutions']:
@@ -17,8 +19,10 @@ for author in authors:
 a = open('authors_names.txt', 'w')
 with open('authors.tex', 'w') as f:
     f.write('\\author{')
+    i = 1
     for author in authors:
-        a.write('%s\n' % author[1]['fullname'])
+        a.write('%i - %s\n' % (i, author[1]['fullname']))
+        i += 1
         if author is authors[-1]:
             endofline = '\n'
         else:
@@ -56,6 +60,21 @@ with open('authors.tex', 'w') as f:
     for author in authors:
         if author[1]['acknowledgements'] != "":
             f.write('%s\n' % author[1]['acknowledgements'])
+
+    f.write('\n')
+    f.write('\n')
+
+    f.write('\\section{Authors Affiliations}\\label{ap:affiliations}\n')
+    for institution in institutions:
+        f.write('\n')
+        f.write('\\noindent\n')
+        f.write('$^{%i}$ %s, %s, %s, %s, %s\n' %
+                (institutions.index(institution) + 1,
+                 authors_json['institutions'][institution]['name'],
+                 authors_json['institutions'][institution]['addressline'],
+                 authors_json['institutions'][institution]['city'],
+                 authors_json['institutions'][institution]['postcode'],
+                 authors_json['institutions'][institution]['country']))
 
 
 a.close()
