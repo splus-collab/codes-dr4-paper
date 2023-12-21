95bdc5dea41cc667ec5420659edcbdcff317eb1f
herpichfr <fabiorafaelh@gmail.com>
2023-12-21 01:17:06 +0000

    Load photometry catalogues to compare auto and psf. Add initial plot

--- codes/compare_auto-psf.py
+++ codes/compare_auto-psf.py
@@ -4,13 +4,18 @@ import os
 import json
 from splusdata import splusdata
 from astropy.coordinates import SkyCoord
+import astropy.units as u
 import argparse
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='Get S-PLUS stamp')
     parser.add_argument('--workdir', type=str,
                         help='Working directory', default=os.getcwd())
+    parser.add_argument('--getstamp', action='store_true',
+                        help='Get the stamp from S-PLUS')
     parser.add_argument('--ra', type=str,
                         help='RA of the object', default='21 33 27.02')
     parser.add_argument('--dec', type=str,
@@ -21,6 +26,8 @@ def get_args():
                         help='Filter of the stamp')
     parser.add_argument('--stamp_name', type=str,
                         help='Name of the stamp', default='stamp.fits')
+    parser.add_argument('--field', type=str,
+                        help='Photometry file', default='photometry.csv')
 
     args = parser.parse_args()
     return args
@@ -61,8 +68,6 @@ def get_splus_stamp(args):
             conn = splusdata.Core()
 
         coords = SkyCoord(ra=args.ra, dec=args.dec, unit=('hourangle', 'deg'))
-        import pdb
-        pdb.set_trace()
 
         hdus_stamp = conn.stamp(coords.ra.value, coords.dec.value, 30, "r")
 
@@ -76,9 +81,74 @@ def get_splus_stamp(args):
     return hdus_stamp
 
 
+def get_auto_photometry(args):
+    dfautor = pd.read_csv(os.path.join(
+        args.workdir, f'dual_PStotal/{args.field}_r.csv'))
+    dfautoi = pd.read_csv(os.path.join(
+        args.workdir, f'dual_PStotal/{args.field}_i.csv'))
+
+    mask = (dfautor['r_PStotal'] > 0) & (dfautoi['i_PStotal'] > 0)
+    mask &= (dfautor['r_PStotal'] < 30) & (dfautoi['i_PStotal'] < 30)
+
+    dfauto = pd.concat([dfautor[mask].reset_index(drop=True),
+                        dfautoi[mask].reset_index(drop=True)],
+                       axis=1)
+
+    return dfauto
+
+
+def get_psf_photometry(args):
+    dfpsfr = pd.read_csv(os.path.join(
+        args.workdir, f'psf_psf/{args.field}_r.csv'))
+    dfpsfi = pd.read_csv(os.path.join(
+        args.workdir, f'psf_psf/{args.field}_i.csv'))
+
+    coords_r = SkyCoord(ra=dfpsfr['RA_r'],
+                        dec=dfpsfr['DEC_r'], unit=('deg', 'deg'))
+    coords_i = SkyCoord(ra=dfpsfi['RA_i'],
+                        dec=dfpsfi['DEC_i'], unit=('deg', 'deg'))
+
+    idx, d2d, _ = coords_r.match_to_catalog_3d(coords_i)
+    sep_constraint = d2d < 1 * u.arcsec
+
+    matched_dfpsfr = dfpsfr[sep_constraint]
+    matched_dfpsfi = dfpsfi.iloc[idx][sep_constraint]
+
+    mask = (matched_dfpsfr['r_psf'] > 0).values & (
+        matched_dfpsfi['i_psf'] > 0).values
+    mask &= (matched_dfpsfr['r_psf'] < 30).values & (
+        matched_dfpsfi['i_psf'] < 30).values
+
+    dfpsf = pd.concat([matched_dfpsfr[mask].reset_index(drop=True),
+                      matched_dfpsfi[mask].reset_index(drop=True)], axis=1)
+
+    return dfpsf
+
+
+def plot_photometry(args):
+
+    dfauto = get_auto_photometry(args)
+    dfpsf = get_psf_photometry(args)
+    import pdb
+    pdb.set_trace()
+
+    fig, ax = plt.subplots(2, 2, figsize=(10, 10))
+    ax.ravel()
+    ax[0, 0].scatter(dfauto['RA'], dfauto['DEC'], s=1, c='k')
+    ax[0, 1].scatter(dfpsf['RA_r'], dfpsf['DEC_r'], s=1, c='k')
+    ax[1, 0].scatter(dfauto['r_PStotal'] - dfauto['i_PStotal'],
+                     dfauto['r_PStotal'], s=1, c='k')
+    ax[1, 1].scatter(dfpsf['r_psf'] - dfpsf['i_psf'],
+                     dfpsf['r_psf'], s=1, c='k')
+    plt.show()
+
+
 def main(args):
-    hdus_stamp = get_splus_stamp(args)
-    print(hdus_stamp)
+    if args.getstamp:
+        hdus_stamp = get_splus_stamp(args)
+        print(hdus_stamp)
+
+    plot_photometry(args)
 
 
 if __name__ == '__main__':
